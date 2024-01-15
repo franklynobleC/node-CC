98b18600f21753997d75a1d0084a37539a23c783
franklynobleC <franklyessien01@gmail.com>
2024-01-15 15:16:02 +0100

    add finished butter framework

--- http-simple-web/butter.js
+++ http-simple-web/butter.js
@@ -26,6 +26,29 @@ class Butter {
         //pipe  to  stream to response
         fileStream.pipe(res)
       }
+
+      //set the Status Code Of the Response
+      res.status = code => {
+        res.statusCode = code
+        return res
+      }
+
+      //set content Type and Return, and  send JSON Data
+      //Send    a json Data   back to The Client    (for small,  json  data, less than  the HighWaterMark Value(highwaterMark Value:  is The Size Of The Stream))
+      res.json = data => {
+        res.setHeader('Content-Type', 'application/json')
+        res.end(JSON.stringify(data))
+      }
+
+      //        if  the routes Object does  not  have a key  of  req.method     + req.url, return  404
+
+      if (!this.routes[req.method.toLocaleLowerCase() + req.url]) {
+        return res
+          .status(404)
+          .json({ error: `Cannot  ${req.method}  ${req.url}` })
+      }
+
+      //else  return  this
       this.routes[req.method.toLocaleLowerCase() + req.url](req, res)
     })
   }
--- http-simple-web/simple-web/server.js
+++
@@ -1,8 +0,0 @@
-const Butter = require('./butter')
-
-const PORT = 4060
-
-const server = new Butter()
-server.listen(PORT, () => {
-  console.log(`server has  started   on  port  ${PORT}`)
-})
